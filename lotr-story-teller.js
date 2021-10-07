b10f95e37b18480e5601136b5e738b6514d43ce9
gregz07 <pedrogregomen@hotmail.com>
2021-10-07 21:54:44 +0100

    Revert "Couple of updates:"
    This reverts commit 81b933a318c1d419c2088cbabb1aeaa9eeb08de4.

--- lotr-story-teller.js
+++ lotr-story-teller.js
@@ -11,85 +11,65 @@ const print = require('./utils/print');
 const embededToken = path.resolve(config.get('bot_token'));
 const prefix = config.get('prefix');
 
-let bot = new Discord.Client();
-let instances = [];
+// private vars
+const _bot = new Discord.Client();
+let connection = undefined;
+let storyTime = false;
 
-bot.once('ready', () => {
-  console.log('Im up Im up!');
+_bot.once('ready', () => {
+  console.log(process.ppid);
+	console.log('Ready!');
 });
 
-bot.on('message', async (message) => {
-  if (!message.member.voice.channel) {
-    print(channel, "ERR_NOT_IN_VOICE_CHANNEL");
-    return;
-  }
-
+_bot.on('message', async function(message){
+  
   let [lotr, ...args] = message.content.split(' ');
   args = args.join(' ');
   
   if (!lotr || lotr !== prefix) {
     return; // Ignore any command
   }
-
-  // setup a new connection
+  
   let channel = message.channel;
 
-  if ((conn = instances.find(i => i.channelId === channel.id))) {
-    // Returns null, posted an issue in discord.js waiting for feedback
-    if (!conn.storyTeller.voice.speaking) {
-      try {
-        let query = commandParser(args);
-        // Find our story 
-        let storyUrl = await analyzer(query);
-        // Story time
-        const dispatcher = storyTeller.play(ytdl(storyUrl, {filter: 'audioonly'}));
-        
-        dispatcher.on('start', function() {
-          print(channel, "STORY_STARTING");
-        });
-    
-        dispatcher.on('finish', function() {
-          print(channel, "STORY_ENDED");
-        });
-      } catch(err) {
-        print(channel, err.message || err);
-        return;
-      }
-    } else {
-      print(channel, "ERR_STORY_UNDERWAY");
-      return;
-    }
+  if (!message.member.voice.channel) {
+    print(channel, "ERR_NOT_IN_VOICE_CHANNEL");
+    return;
   }
-  else {
-    let storyTeller = await message.member.voice.channel.join();
-    
-    try {
-      let query = commandParser(args);
-      // Find our story 
-      let storyUrl = await analyzer(query);
-      // Story time
-      const dispatcher = storyTeller.play(ytdl(storyUrl, {filter: 'audioonly'}));
-      
-      dispatcher.on('start', function() {
-        print(channel, "STORY_STARTING");
-      });
-  
-      dispatcher.on('finish', function() {
-        print(channel, "STORY_ENDED");
-      });
-    } catch(err) {
-      print(channel, err.message || err);
-      return;
-    }
 
-    instances.push({
-      storyTeller: storyTeller,
-      channelId: channel.id,
+  if (!connection) {
+    connection = await message.member.voice.channel.join();
+  }
+
+  if (storyTime) {
+    print(channel, "ERR_STORY_UNDERWAY");
+    return;
+  }
+
+  try {
+    let query = commandParser(args);
+    // Find our story 
+    let storyUrl = await analyzer(query);
+    // Story time
+    console.log(storyUrl)
+    const dispatcher = connection.play(ytdl(storyUrl, {filter: 'audioonly'}));
+    console.log('here')
+    dispatcher.on('start', function() {
+      print(channel, "STORY_STARTING");
+      storyTime = true;
     });
+    dispatcher.on('finish', function() {
+      print(channel, "STORY_ENDED");
+      storyTime = false;
+    });
+  } catch(err) {
+    print(channel, err.message || err);
+    return;
   }
 });
 
-bot.on('disconnect', (evt) => console.log(`Bot disconnected`));
+_bot.on('disconnect', function(evt) {
+  console.log("disconnected")
+})
 
-bot.login(fs.readFileSync(embededToken, 'utf-8'));
-
+_bot.login(fs.readFileSync(embededToken, 'utf-8'));
